781cf51696ffa95a8ecaf0df76e16aa4571052f6
harley yefrey cabrales vargas <arleycabralesvargas@gmail.com>
2025-02-03 09:28:53 -0500

    Add files via upload

---
+++ Python_CabralesHarley.code/ApellidosNombres.py/a.py
@@ -0,0 +1,92 @@
+nombres = [
+    ["Adrián"],
+    ["Alejandra"],
+    ["Ámbar", "Isabella"],
+    ["Andrés", "David"],
+    ["Aura", "Camila"],
+    ["Camilo", "Andrés"],
+    ["Daniel", "Esteban"],
+    ["David", "Santiago"],
+    ["Edgar", "Leonardo"],
+    ["Gerson", "Steven"],
+    ["Harley", "Yefrey"],
+    ["John", "Stiven"],
+    ["Juan", "David"],
+    ["Juan", "David"],
+    ["Juan", "David"],
+    ["Juan", "Eduardo"],
+    ["Juan", "Fernando"],
+    ["Juan", "Jose"],
+    ["Maria", "Juliana"],
+    ["Mateo", "Roman"],
+    ["Naya", "Zarela"],
+    ["Nicolas"],
+    ["Omar", "Fernando"],
+    ["Santiago"],
+    ["Santiago", "Andrés"],
+    ["Santiago"],
+    ["Sara", "Sofía"],
+    ["Sayara", "Yurley"],
+    ["Sergio", "Andrés"],
+    ["Simón", "Dante"],
+    ["Thomas", "Sebastián"],
+    ["Vladimir"]
+]
+
+apellidos = [
+    ["Quintero", "Pinzón"],
+    ["Pinzón", "Alvarez"],
+    ["Plata", "López"],
+    ["Reyes", "Espinel"],
+    ["Pico", "Araque"],
+    ["Suárez", "Fuentes"],
+    ["Guerrero", "Quintero"],
+    ["Vera", "Mendez"],
+    ["Acevedo", "Arteaga"],
+    ["Chaparro", "Martínez"],
+    ["Cabrales", "Vargas"],
+    ["Negron", "Regalado"],
+    ["Saavedra", "Jaimez"],
+    ["Santoyo", "Jaimes"],
+    ["Vargas", "Soto"],
+    ["Pinilla", "Guzmán"],
+    ["Umaña", "Barragan"],
+    ["Abril", "Roman"],
+    ["Saavedra", "Mejia"],
+    ["Camargo"],
+    ["Lizcano", "Jaimes"],
+    ["Chedraui", "Mantilla"],
+    ["Granados", "Parra"],
+    ["Aguilar", "Vesga"],
+    ["Quiñonez", "Sosa"],
+    ["Jaimes", "Perez"],
+    ["Díaz", "Rodríguez"],
+    ["Aparicio", "Arciniegas"],
+    ["Rueda", "Hernández"],
+    ["Salamanca", "Galvis"],
+    ["Bastos", "Garcia"],
+    ["Diaz", "Contreras"]
+]
+
+for i in range (len(nombres)):
+    print("Estudiante ",i+1, " ",nombres[i]," ",apellidos[i])
+a=True
+while a==True:
+    c=int(input("Cual estudiante desea modificar: "))
+    s=int(input("Si desea modificar nombre presione 1, si desea modificar apellido presione 2: "))
+    if s==1:
+        k=input("Cual es el nuevo nombre que desea editar: ")
+        nombres[c-1]=[k]
+    elif s==2:
+        g=input("Cual es el nuevo apellido que desea editar")
+        apellidos[c-1]=[g]
+    d=int(input("Desea seguir editando 1:si 2:no : "))
+    if d==2:
+        a=False  
+for i in range (len(nombres)):
+    print("Estudiante ",i+1, " ",nombres[i]," ",apellidos[i])
+
+
+
+
+     
